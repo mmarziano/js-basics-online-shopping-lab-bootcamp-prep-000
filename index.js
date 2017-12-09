f8c8b98b619f37393b5bdc9abcc51391eaf7ab4a
Monica Marziano <mmarziano1@gmail.com>
2017-12-09 11:46:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
   var itemPrice = Math.floor(Math.random()*100)+1;
-  var item = Object.assign({},{[itemName]:itemPrice});
+  const item = Object.assign({},{[itemName]:itemPrice});
 
  cart.push(item);
  return cart;
