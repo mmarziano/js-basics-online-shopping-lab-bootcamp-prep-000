cebe5052498d82ce7c85634ec9a292add7a79442
Monica Marziano <mmarziano1@gmail.com>
2017-12-09 11:33:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  var itemPrice = Math.floor(Math.random())+1;
+  var itemPrice = Math.floor(Math.random()*100)+1;
   var item = {itemName: itemPrice};
 
  cart.push(item);
